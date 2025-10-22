eaab1025e7e81cabd670dae4a8ea2e56d09aa1ce
Nathen Bowes <nbowes17@gmail.com>
2025-10-22 10:59:31 -0700

    refactor table of contents to before block

--- book_viewer.rb
+++ book_viewer.rb
@@ -2,16 +2,18 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubi"
 
+before do
+  @toc = File.readlines("data/toc.txt")
+end
+
 get "/" do
   @title = "The Adventures of Sherlocks Home"
-  @toc = File.readlines("data/toc.txt")
 
   erb :home
 end
 
 get "/chapters/:number" do
   @chapter_num = params["number"]
-  @toc = File.readlines("data/toc.txt")
   @chapter_name = @toc[@chapter_num.to_i - 1]
   @title = "Chapter #{@chapter_num}: #{@chapter_name}"
   @chapter_content = File.read("data/chp#{@chapter_num}.txt")
