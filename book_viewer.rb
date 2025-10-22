03df47e3db7ff407c2ed6d968192c223f7556b44
Nathen Bowes <nbowes17@gmail.com>
2025-10-22 11:31:42 -0700

    add redirect for invalid urls + invalid chapters

--- book_viewer.rb
+++ book_viewer.rb
@@ -15,10 +15,20 @@ end
 get "/chapters/:number" do
   @chapter_num = params["number"]
   @chapter_name = @toc[@chapter_num.to_i - 1]
+
+  # works similar to inlcude, but doesn't iterate
+  # through every element, so it is faster, especially
+  # for non-numeric types
+  redirect "/" unless (1..@toc.size).cover?(@chapter_num)
+
   @title = "Chapter #{@chapter_num}: #{@chapter_name}"
   @chapter_content = File.read("data/chp#{@chapter_num}.txt")
 
   erb :chapter
+end
+
+not_found do
+  redirect "/"
 end
 
 helpers do
