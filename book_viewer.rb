8c4aa4625363b800d702d6fca68bc545164bdd52
Nathen Bowes <nbowes17@gmail.com>
2025-10-22 12:44:53 -0700

    add search functionality + view

--- book_viewer.rb
+++ book_viewer.rb
@@ -3,7 +3,7 @@ require "sinatra/reloader"
 require "tilt/erubi"
 
 before do
-  @toc = File.readlines("data/toc.txt")
+  @chapters = File.readlines("data/toc.txt")
 end
 
 get "/" do
@@ -13,13 +13,14 @@ get "/" do
 end
 
 get "/chapters/:number" do
+  number =  params["number"].to_i
   @chapter_num = params["number"]
-  @chapter_name = @toc[@chapter_num.to_i - 1]
+  @chapter_name = @chapters[number - 1]
 
   # works similar to inlcude, but doesn't iterate
   # through every element, so it is faster, especially
   # for non-numeric types
-  redirect "/" unless (1..@toc.size).cover?(@chapter_num)
+  redirect "/" unless (1..@chapters.size).cover?(number)
 
   @title = "Chapter #{@chapter_num}: #{@chapter_name}"
   @chapter_content = File.read("data/chp#{@chapter_num}.txt")
@@ -27,8 +28,26 @@ get "/chapters/:number" do
   erb :chapter
 end
 
+get "/search" do
+  @search = params[:query]
+
+  @results = chapters_matching(@search)
+  erb :search
+end
+
 not_found do
   redirect "/"
+end
+
+def chapters_matching(query)
+  results = []
+  return results if !query || query.empty?
+
+  @chapters.each_with_index do |chapter, index|
+    results << {number: index + 1, name: chapter} if chapter.include?(query)
+  end
+
+  results
 end
 
 helpers do
